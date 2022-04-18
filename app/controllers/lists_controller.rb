bee53047c553c8166cf0321f8bef73437351de9b
nofaralfasi <nofaralfasi@gmail.com>
2022-04-18 18:11:52 +0300

    Added functionality to delete lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -33,7 +33,18 @@ class ListsController < ApplicationController
     end
   end
 
+  def delete
+    @list = List.find(params[:id])
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to(lists_path)
+  end
+
   private
+
   def list_params
     params.require(:list).permit(:title)
   end
