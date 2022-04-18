a5268314bf0c823f9b7644f80509a0c13def9f30
nofaralfasi <nofaralfasi@gmail.com>
2022-04-18 14:41:22 +0300

    added resources to routes.rb - not tested yet

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,17 @@ Rails.application.routes.draw do
   get 'lists/index'
   get 'labels/index'
   get 'tasks/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :lists do
+    member do
+      get :delete
+    end
+  end
+
+  resources :tasks do
+    member do
+      get :delete
+    end
+  end
+
 end
