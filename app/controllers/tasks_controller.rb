83620295e270d49c00bd401b3a2f48657a67ce49
nofaralfasi <nofaralfasi@gmail.com>
2022-04-19 19:29:43 +0300

    Added functionality for the creation of new tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,27 @@
 class TasksController < ApplicationController
   def index
   end
+
+  def new
+    @task = Task.new
+    @task.list_id = cookies[:list_id]
+    # @list = List.find(flash[:list_id])
+  end
+
+  def create
+    # @list = List.find(flash[:list_id])
+    @task = Task.new(task_params)
+    @task.list_id = cookies[:list_id]
+    # @task.label_id = 0
+    if @task.save
+      # redirect_to(lists_path)
+      redirect_to(list_path(cookies[:list_id]))
+    else
+      render('new')
+    end
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :due_date, :priority, :status)
+  end
 end
--- db/migrate/20220418110641_create_tasks.rb
+++ db/migrate/20220418110641_create_tasks.rb
@@ -5,7 +5,7 @@ class CreateTasks < ActiveRecord::Migration[7.0]
       t.belongs_to :label
       t.string :title
       t.string :due_date
-      t.string :priority
+      t.integer :priority
       t.boolean :status
 
       t.timestamps
