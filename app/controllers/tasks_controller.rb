730f4941251314fc12162a3580bdccfc3bac5c61
nofaralfasi <nofaralfasi@gmail.com>
2022-04-20 13:21:52 +0300

    finished functionality for the task_controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,17 +2,19 @@ class TasksController < ApplicationController
   def index
   end
 
+  def show
+    @task = Task.find(params[:id])
+  end
+
   def new
     @task = Task.new
     @task.list_id = cookies[:list_id]
-    # @list = List.find(flash[:list_id])
   end
 
   def create
-    # @list = List.find(flash[:list_id])
     @task = Task.new(task_params)
     @task.list_id = cookies[:list_id]
-    # @task.label_id = 0
+    @task.label_id = 0
     if @task.save
       # redirect_to(lists_path)
       redirect_to(list_path(cookies[:list_id]))
@@ -21,7 +23,30 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to(task_path(@task))
+    else
+      render('edit')
+    end
+  end
+
+  def delete
+    @task = Task.find(params[:id])
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to(list_path(cookies[:list_id]))
+  end
+
   def task_params
-    params.require(:task).permit(:title, :due_date, :priority, :status)
+    params.require(:task).permit(:title, :due_date, :priority, :status, :label_id)
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
   belongs_to :list
   has_many :labels
+#  TODO change back to belongs_to :label
 end
