b5cacb70a51d5f3f892f51457939212df0a1676a
nofaralfasi <nofaralfasi@gmail.com>
2022-04-25 17:16:20 +0300

    added new functionality to display errors

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,14 +1,11 @@
 class ListsController < ApplicationController
+  before_action :find_list, only: [:show, :edit, :update, :delete, :destroy]
+
   def index
     @lists = List.all
   end
 
   def show
-    @list = List.find_by(id: params[:id])
-
-    if @list.blank?
-      redirect_to(lists_path, notice: "List #{params[:id]} was not found.")
-    end
   end
 
   def new
@@ -26,20 +23,9 @@ class ListsController < ApplicationController
   end
 
   def edit
-    @list = List.find_by_id(params[:id])
-
-    if @list.blank?
-      redirect_to(lists_path, notice: "List #{params[:id]} was not found.")
-    end
   end
 
   def update
-    @list = List.find(params[:id])
-
-    if @list.blank?
-      redirect_to(lists_path, notice: "List #{params[:id]} was not found.")
-    end
-
     if @list.update(list_params)
       redirect_to(list_path(@list))
     else
@@ -48,22 +34,19 @@ class ListsController < ApplicationController
   end
 
   def delete
-    @list = List.find_by(id: params[:id])
-
-    if @list.blank?
-      redirect_to(lists_path, notice: "List #{params[:id]} was not found.")
-    end
   end
 
   def destroy
+    @list.destroy
+    redirect_to(lists_path)
+  end
+
+  def find_list
     @list = List.find_by(id: params[:id])
 
     if @list.blank?
       redirect_to(lists_path, notice: "List #{params[:id]} was not found.")
     end
-
-    @list.destroy
-    redirect_to(lists_path)
   end
 
   private
